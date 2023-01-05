ce12e8017d12def0a0f50a3522995acde958aea0
Pang-GJ <2362421192@qq.com>
2023-01-06 00:07:33 +0800

    chore: add check-lint, check-clang-tidy

--- xmake.lua
+++ xmake.lua
@@ -12,3 +12,20 @@ add_rules("mode.release", "mode.debug")
 
 -- inclue subdirs
 includes("toykv", "tests", "example")
+
+-- run script
+target("check-lint")
+  set_kind("phony")
+
+  on_run(function (target)
+    os.run("sh $(projectdir)/script/check_lint.sh")
+  end)
+
+target("check-tidy")
+  set_kind("phony")
+
+  on_run(function (target)
+    os.run("xmake project -k compile_commands")
+    --[[ os.run("sh $(projectdir)/script/check_tidy.sh") ]]
+    os.run("python $(projectdir)/script/run_clang_tidy.py -p ./ ")
+  end)
